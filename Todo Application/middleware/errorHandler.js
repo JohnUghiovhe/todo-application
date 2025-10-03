7140326cf1a4fdacd2054d3de589984b3da84bfa
John Ughiovhe <johnandrewjaw@gmail.com>
2025-10-03 23:58:16 +0100

    Delete Todo Application directory

--- Todo Application/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-function errorHandler(err, req, res, next) {
-  console.error(`[${new Date().toISOString()}] Error:`, err.message);
-  res.status(500).render('error', { error: err });
-}
-
-module.exports = errorHandler;
